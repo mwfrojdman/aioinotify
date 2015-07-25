812fede6d8e98b2d0f6d70a3702a083e4fe5ef8e
Markus Törnqvist <markus.tornqvist@codento.com>
2015-07-25 16:55:48 +0300

    Examples

---
+++ examples/getting_started.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import asyncio
+from aioinotify import connect_inotify
+
+
+@asyncio.coroutine
+def print_event(event):
+    print('Got event: {}'.format(event))
+
+
+@asyncio.coroutine
+def watch_directory(path):
+    transport, inotify = yield from connect_inotify()
+    watch = yield from inotify.watch(print_event, path, all_events=True)
+    print('Printing all file system events in {}'.format(path))
+    yield from asyncio.sleep(60.0)
+
+    print('And now his watch is ended')
+    watch.close()
+    inotify.close()
+    transport.close()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(watch_directory(sys.argv[1]))
+    loop.close()
+
